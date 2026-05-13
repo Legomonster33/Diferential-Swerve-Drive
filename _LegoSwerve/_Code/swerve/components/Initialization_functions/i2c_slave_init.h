a2565571c42425a99bb8de4591b95cadf688bc98
Legomonster33 <siglevan@gmail.com>
2026-05-13 15:43:02 -0400

    note

--- _LegoSwerve/_Code/swerve/components/Initialization_functions/i2c_slave_init.h
+++ _LegoSwerve/_Code/swerve/components/Initialization_functions/i2c_slave_init.h
@@ -14,3 +14,7 @@
 void i2c_new_slave_init(uint8_t slave_addr, wheel_data_t *wheel_data_ptr);
 
 #endif // I2C_SLAVE_INIT_H
+
+
+
+// if this doesnt work, try writing to the i2c buffer in the main loop instead of using the slave callback, and see if the data is correctly read by the master. If it works, then the issue is with the slave callback function, and we can debug it further. If it doesn't work, then the issue might be with the i2c configuration or wiring.
